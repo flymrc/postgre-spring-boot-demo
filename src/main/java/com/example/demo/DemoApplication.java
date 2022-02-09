87a4dc56f884fbf875e4a5cbca940a0fa44b0e7c
Ruichao Ma <ruichaoma@pixelshift.ai>
2022-02-09 18:11:48 +0800

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -16,8 +23,9 @@ import java.util.UUID;
 public class DemoApplication {
   private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
-  public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+  @RequestMapping("/user")
+  public Principal user(Principal user) {
+    return user;
   }
 
   @RequestMapping("/resource")
@@ -26,5 +34,27 @@ public class DemoApplication {
     model.put("id", UUID.randomUUID().toString());
     model.put("content", "Hello World");
     return model;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Configuration
+  @Order(SecurityProperties.BASIC_AUTH_ORDER)
+  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+        .antMatchers("/resources/**").permitAll()
+        .antMatchers("/*.js", "/*.css").permitAll()
+        .anyRequest().authenticated()
+        .and().csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
   }
 }
