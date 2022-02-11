034ce34033ad6fbebff6e95f02890bac971c9435
Ruichao Ma <ruichaoma@pixelshift.ai>
2022-02-11 15:21:32 +0800

    init

--- resource/src/main/java/com/example/demo/ResourceApplication.java
+++ resource/src/main/java/com/example/demo/ResourceApplication.java
@@ -2,12 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +10,7 @@ import java.util.UUID;
 
 @SpringBootApplication
 @RestController
-@EnableRedisHttpSession
-public class ResourceApplication extends WebSecurityConfigurerAdapter {
+public class ResourceApplication {
 
   @RequestMapping("/")
   @CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = {"x-auth-token", "x-requested-with", "x-xsrf-token"})
@@ -28,25 +21,11 @@ public class ResourceApplication extends WebSecurityConfigurerAdapter {
   public static void main(String[] args) {
     SpringApplication.run(ResourceApplication.class, args);
   }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.cors()
-      .and().authorizeRequests()
-      .anyRequest().authenticated()
-      .and().sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.NEVER);
-  }
-
-  @Bean
-  HeaderHttpSessionIdResolver sessionStrategy() {
-    return new HeaderHttpSessionIdResolver("x-auth-token");
-  }
 }
 
 class Message {
   private final String id = UUID.randomUUID().toString();
-  private String content;
+  private final String content;
 
   public Message(String content) {
     this.content = content;
@@ -58,9 +37,5 @@ class Message {
 
   public String getContent() {
     return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
   }
 }
