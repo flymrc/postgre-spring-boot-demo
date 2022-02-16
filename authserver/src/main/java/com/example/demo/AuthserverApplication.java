9994b456f512653cb00abab79b72e2d440eda038
Ruichao Ma <ruichaoma@pixelshift.ai>
2022-02-16 14:45:50 +0800

    update

--- authserver/src/main/java/com/example/demo/AuthserverApplication.java
+++ authserver/src/main/java/com/example/demo/AuthserverApplication.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class AuthserverApplication {
+@EnableAuthorizationServer
+@Controller
+public class AuthserverApplication extends WebMvcConfigurerAdapter {
+
+  @RequestMapping("/user")
+  @ResponseBody
+  public Principal user(Principal user) {
+    return user;
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthserverApplication.class, args);
 	}
-
 }
