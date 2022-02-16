07bc54a9c8472cdb4d736e670b1b9f997296fb3c
Ruichao Ma <ruichaoma@pixelshift.ai>
2022-02-16 15:25:01 +0800

    update

--- resource/src/main/java/com/example/demo/ResourceApplication.java
+++ resource/src/main/java/com/example/demo/ResourceApplication.java
@@ -2,9 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +11,7 @@ import java.util.UUID;
 
 @SpringBootApplication
 @RestController
+@EnableResourceServer
 public class ResourceApplication extends WebSecurityConfigurerAdapter {
 
   @RequestMapping("/")
@@ -21,12 +21,6 @@ public class ResourceApplication extends WebSecurityConfigurerAdapter {
 
   public static void main(String[] args) {
     SpringApplication.run(ResourceApplication.class, args);
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic().disable();
-    http.authorizeRequests().anyRequest().authenticated();
   }
 }
 
