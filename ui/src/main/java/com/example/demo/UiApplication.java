b2e2f35f37a526344f77566e10b0f4fb9c93eeb6
Ruichao Ma <ruichaoma@pixelshift.ai>
2022-02-17 16:48:07 +0800

    update

--- ui/src/main/java/com/example/demo/UiApplication.java
+++ ui/src/main/java/com/example/demo/UiApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
@@ -18,7 +16,6 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableZuulProxy
 @EnableOAuth2Sso
 public class UiApplication extends WebSecurityConfigurerAdapter {
-  private static final Logger log = LoggerFactory.getLogger(UiApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(UiApplication.class, args);
@@ -29,7 +26,7 @@ public class UiApplication extends WebSecurityConfigurerAdapter {
     http
       .logout().logoutSuccessUrl("/").and()
       .authorizeRequests()
-      .antMatchers("/index.html", "/", "/app.html").permitAll()
+      .antMatchers("/index.html", "/").permitAll()
       .antMatchers("/*.js", "/*.css", "/favicon.ico").permitAll()
       .anyRequest().authenticated().and()
       .csrf()
