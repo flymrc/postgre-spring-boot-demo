0715535c0f7e1ead1d84317e5ca1c392a8f4ab20
Ruichao Ma <ruichaoma@pixelshift.ai>
2022-02-18 20:23:35 +0800

    update

--- authserver/src/test/java/com/example/demo/AuthserverApplicationTests.java
+++ authserver/src/test/java/com/example/demo/AuthserverApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.demo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class AuthserverApplicationTests {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-	@Test
-	void contextLoads() {
-	}
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AuthserverApplicationTests {
+
+  @LocalServerPort
+  private int port;
+
+  private TestRestTemplate template = new TestRestTemplate();
+
+  @Test
+  public void homePageProtected() {
+    ResponseEntity<String> response = template.getForEntity("http://localhost:" + port + "/uaa/", String.class);
+    assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    String auth = response.getHeaders().getFirst("WWW-Authenticate");
+    assertTrue("Wrong header: " + auth, auth.startsWith("Bearer realm=\""));
+  }
+
+  @Test
+  public void userEndpointProtected() {
+    ResponseEntity<String> response = template.getForEntity("http://localhost:" + port + "/uaa/user", String.class);
+    assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    String auth = response.getHeaders().getFirst("WWW-Authenticate");
+    assertTrue("Wrong header: " + auth, auth.startsWith("Bearer realm=\""));
+  }
+
+  @Test
+  public void authorizationRedirects() {
+    ResponseEntity<String> response = template.getForEntity("http://localhost:" + port + "/uaa/oauth/authorize", String.class);
+    assertEquals(HttpStatus.FOUND, response.getStatusCode());
+    String location = response.getHeaders().getFirst("Location");
+    assertTrue("Wrong header: " + location, location.startsWith("http://localhost:" + port + "/uaa/login"));
+  }
 
 }
