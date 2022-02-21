f8495787a08cb1219592468378ce62bafefeb716
ruichao.ma <flymrc@gmail.com>
2022-02-21 16:06:39 +0800

    spring security jwt (#9)
    * init
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    Co-authored-by: Ruichao Ma <ruichaoma@pixelshift.ai>

--- resource/src/test/java/com/example/demo/ResourceApplicationTests.java
+++ resource/src/test/java/com/example/demo/ResourceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class ResourceApplicationTests {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ResourceApplicationTests {
+
+  @LocalServerPort
+  private int port;
+
+  private TestRestTemplate template = new TestRestTemplate();
 
 	@Test
-	void contextLoads() {
+	public void resourceLoads() {
+    ResponseEntity<String> response = template.getForEntity("http://localhost:{port}/resource/", String.class, port);
+    assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    String auth = response.getHeaders().getFirst("WWW-Authenticate");
+    assertTrue("Wrong header: " + auth , auth.startsWith("Bearer"));
 	}
-
 }
