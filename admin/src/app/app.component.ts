3a62f16eb9677a70c301c766523d97fb629f9b4f
Ruichao Ma <ruichaoma@pixelshift.ai>
2022-02-22 12:07:13 +0800

    update

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import 'rxjs/add/operator/finally';
 
 @Component({
   selector: 'app-root',
