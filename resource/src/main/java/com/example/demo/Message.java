3645d89a73cddfeb1c4547076d4d756d77422f82
Ruichao Ma <ruichaoma@pixelshift.ai>
2022-02-24 19:48:55 +0800

    update

---
+++ resource/src/main/java/com/example/demo/Message.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.UUID;
+
+class Message {
+  private String id = UUID.randomUUID().toString();
+  private String content;
+
+  Message() {
+  }
+
+  public Message(String content) {
+    this.content = content;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
